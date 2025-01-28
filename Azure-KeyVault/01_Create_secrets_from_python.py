1af3232973dadc8978ba67da34463a93eea2b802
Yaseen Banu <36068739+yaseenbanu@users.noreply.github.com>
2025-01-28 12:34:21 +0530

    Update 01_Create_secrets_from_python.py

--- Azure-KeyVault/01_Create_secrets_from_python.py
+++ Azure-KeyVault/01_Create_secrets_from_python.py
@@ -9,22 +9,11 @@ credential = DefaultAzureCredential()
 client = SecretClient(vault_url=KVUri, credential=credential)
 
 secretName = 'azurestoragedatasets-connection-strings'
-secretValue = 'secret_value'
+s_Value = 'secret_value'
 
-print(f"Creating a secret in KV_NAME called '{secretName}' with the value '{secretValue}' ...")
+print(f"Creating a secret in KV_NAME called '{secretName}' with the value '{s_Value}' ...")
 
-client.set_secret(secretName, secretValue)
+client.set_secret(secretName, s_Value)
 
 print(" done.")
 
-# print(f"Retrieving your secret from KV_NAME.")
-
-# retrieved_secret = client.get_secret(secretName)
-
-# print(f"Your secret is '{retrieved_secret.value}'.")
-# print(f"Deleting your secret from KV_NAME ...")
-
-# poller = client.begin_delete_secret(secretName)
-# deleted_secret = poller.result()
-
-# print(" done.")
